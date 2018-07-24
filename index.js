b35df2c08896a776b6d1d30c09b0a46cfc84184a
brains1960 <tundunoladipo@gmail.com>
2018-07-24 14:22:58 -0400

    Orignal File Formatting

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+const dialogflow = require('dialogflow');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 // If modifying these scopes, delete credentials.json.
 const SCOPES = ["https://www.googleapis.com/auth/calendar",
@@ -21,6 +28,15 @@ const rtm = new RTMClient(token)
 const express = require('express');
 const app = express();
 
+// DialogFlow Setup ----------------------------------------------------------------
+const Storage = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = new Storage();
+
+//---------------------------------------- End of all Setup ----------------------------
 // Global variables
 let EVENTTOCREATE = {};
 
@@ -90,7 +106,7 @@ app.post('/oauth', (req, res) => {
   }
 })
 
-//------------------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------  Google Calendar API Functions ----------------------------------------------------------------
 
 // Load client secrets from a local file.
 // fs.readFile("credentials.json", (err, content) => {
@@ -193,6 +209,7 @@ function createEvent(auth) {
   });
 }
 
+
 // List Available Times for the next 7 days
 function listAvailable(auth) {
   const startDate = new Date();
@@ -234,3 +251,150 @@ function listAvailable(auth) {
     endDate.setDate(startDate.getDate() + 1);
   }
 }
+
+// ----------------------------------------DialogFlow Functions -----------------------------------
+
+// Makes an authenticated API request.
+storage
+  .getBuckets()
+  .then((results) => {
+    const buckets = results[0];
+
+    console.log('Buckets:');
+    buckets.forEach((bucket) => {
+      console.log(bucket.name);
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+
+/// NLP Start!!
+// Define session path
+
+ userRequest = async(inputQuery) => {
+  console.log('inputQuery---->', inputQuery)
+  const projectId = process.env.DIALOGFLOW_PROJECT_ID; //https://dialogflow.com/docs/agents#settings
+  const sessionId = 'quickstart-session-id';
+  const query = inputQuery;
+  const languageCode = 'en-US';
+
+  // Instantiate a DialogFlow client.
+  const sessionClient = new dialogflow.SessionsClient();
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode,
+      },
+    },
+  };
+  console.log("request test ============", request)
+
+  // Send request and log result
+return  sessionClient
+    .detectIntent(request)
+    .then((responses) => {
+      // console log check in to see if request received and being processed
+      console.log('Detected intent');
+      const result = responses[0].queryResult;
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+      console.log(`     DATE: ${(result.parameters.fields.date.stringValue)}`);
+      console.log(`     SUBJECT: ${(result.parameters.fields.subject.stringValue)}`);
+
+      // start and end dates for same day events
+      var startDate = new Date(result.parameters.fields.date.stringValue)
+      var endDate = new Date(startDate.setDate(startDate.getDate() + 1))
+
+
+      if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+        return resultObject = {
+          /////// to be sent to check in with user
+          eventSend: {confirmation: result.fulfillmentText},
+          /////// info for the calendar
+          confirmedMessage: {
+            start:{
+              date: result.parameters.fields.date.stringValue
+            },
+            end: {
+              date:endDate,
+            },
+            description: result.parameters.fields.subject.stringValue,
+            calendarId: "primary"
+          }
+        }
+
+      } else {
+        console.log(`  No intent matched.`);
+      }
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+}
+
+const slack = require('@slack/client')
+const RTMClient = slack.RTMClient
+const WebClient = slack.WebClient
+const token = 'xoxb-402681346384-403536981733-kqaM0ezYN5OdLCXv1T8h6wlU'
+const web = new WebClient(token)
+const rtm = new RTMClient(token)
+rtm.start()
+
+let answer;
+rtm.on('message', (message) => {
+  if ( (message.subtype && message.subtype === 'bot_message') ||
+     (!message.subtype && message.user === rtm.activeUserId) ) {
+       return;
+     }
+
+  userRequest(message.text)
+    .then(answer => { web.chat.postMessage({
+        channel: message.channel,
+        text: `${answer.eventSend.confirmation}, Please confirm!`,
+        attachments: [
+            {
+                "fallback": "You are unable to confirm",
+                "callback_id": "wopr_game",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "response",
+                        "text": "Yes",
+                        "type": "button",
+                        "value": "true",
+                        "style": "primary",
+                    },
+                    {
+                        "name": "response",
+                        "text": "No",
+                        "type": "button",
+                        "value": "false",
+                        "style": "danger",
+                    }
+                ]
+            }
+        ]
+    })})
+    .then(() => console.log('Message sent to channel'))
+    .catch(console.error)
+})
+// app.post('/oauth', (req, res) => {
+//   if (req.payload.actions[0].value === true){
+//     console.log('heyyyy it worked!')
+//     // T runs the google calendar stuff here
+//     res.send({message: 'scheduled!'})
+//   } else {
+//     res.send({message: 'canceled scheuling'})
+//   }
+// })
+
+
+userRequest("schedule a meeting with nick at 3pm tomorrow to talk about coding")
